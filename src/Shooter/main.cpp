0e91b5cac0e509675bc8e0fccac5b1c137008011
kaeloo <147034168+kaeloo8@users.noreply.github.com>
2025-03-10 21:14:30 +0100

    bg

--- src/Shooter/main.cpp
+++ src/Shooter/main.cpp
@@ -4,7 +4,16 @@
 #include <crtdbg.h>
 
 
-int main() 
+int main()
 {
-	return 0;
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
