4bde4fae0e4be9141bc4a293db4f4968404dd7b9
kaeloo <147034168+kaeloo8@users.noreply.github.com>
2025-03-21 16:10:01 +0100

    454545454545455585858585888

--- src/JeuLibre/AssetManager.h
+++ src/JeuLibre/AssetManager.h
@@ -18,11 +18,17 @@ public:
     sf::RenderWindow* Win;
     std::vector<ImageData> images;
 
+
+    std::vector<std::string> AlreadyLoad;
+
     sf::Texture backgroundTexture;
     sf::Sprite backgroundSprite;
 
     sf::RectangleShape progressBarBg;
     sf::RectangleShape progressBar;
+
+    int loadedImg = 0;
+    int sizedirImg = 0;
 
     int sizedir = 0;
     int loaded = 0;
@@ -31,4 +37,6 @@ public:
     sf::Texture& GetTexture(const std::string& path);
     void updateProgressBar();
     void parseTileMap(const std::string& chemin, const std::string& filename);
+    void LoadFromFile(std::vector<std::string>);
+    void LoadFromFile(std::string path);
 };
